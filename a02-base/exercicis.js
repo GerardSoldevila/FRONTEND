fcb7b753a05d7a6a919e2c1711f0ee320d8fc6f4
Gerard Soldevila <gersoldevila@gmail.com>
2024-09-17 19:01:17 +0200

    Activitat 02 OBJECTES

---
+++ a02-base/exercicis.js
@@ -0,0 +1,60 @@
+document.getElementById('ex01').addEventListener("click", function() {
+	
+
+	let script = prompt("Introdueix una cadena de text: ")
+	
+	words = script.split(" ")
+	upperCase = script.toUpperCase()
+	alert("Text: " + script +'\n'+ "Nombre de caracters: " + script.length +'\n'+ "Nombre de paraules: " + words.length +'\n'+ "Text en majúscules: " + upperCase)
+	
+
+});
+
+/***********************************************************************************/
+
+document.getElementById('ex02').addEventListener("click", function() {
+	
+
+
+	
+});
+
+/***********************************************************************************/
+
+document.getElementById('ex03').addEventListener("click", function() {
+	// ------------------
+	// Codi exercici 3...
+	// ------------------
+});
+
+/***********************************************************************************/
+
+document.getElementById('ex04').addEventListener("click", function() {
+	// ------------------
+	// Codi exercici 4...
+	// ------------------
+});
+
+/***********************************************************************************/
+
+document.getElementById('ex05').addEventListener("click", function() {
+	// ------------------
+	// Codi exercici 5...
+	// ------------------
+});
+
+/***********************************************************************************/
+
+document.getElementById('ex06').addEventListener("click", function() {
+	// ------------------
+	// Codi exercici 6...
+	// ------------------
+});
+
+/***********************************************************************************/
+
+document.getElementById('ex07').addEventListener("click", function() {
+	// -------------------
+	// Codi exercici 7...
+	// -------------------
+});
