d8fa4da35f597b6d41dd011086b904a0dad2c3d1
Gerard Soldevila <gersoldevila@gmail.com>
2024-09-18 19:27:42 +0200

    revisio

--- a02-base/exercicis.js
+++ a02-base/exercicis.js
@@ -15,49 +15,79 @@ document.getElementById('ex01').addEventListener("click", function() {
 document.getElementById('ex02').addEventListener("click", function() {
 	
 	let script = prompt("Introdueix una cadena de text: ")
-	let position = parseInt(prompt("Digues una posició numèrica: "))
+	let position = parseInt(prompt("Quina posició vols consultar?: "))
 	
+	while (position < 0 || position >= script.length)  {
+		position = parseInt(prompt("Posicio no vàlida!"+ '\n' + "Digues una posició numèrica vàlida: "))
+	}
+	alert("La posicio de " + position +" de la cadena és " + script[position])
 
-	do {
-		position = prompt("Digues una posició numèrica vàlida: ")
+	});
 
-	} while (position < 0 || position > script.length)
-	
-	newScript = script.split(" ")	
-	alert(newScript[position])	 
-
-});
 
 /***********************************************************************************/
 
 document.getElementById('ex03').addEventListener("click", function() {
-	// ------------------
-	// Codi exercici 3...
-	// ------------------
+	let cadena = prompt("Introdueix una cadena de text: ")
+	let substring = cadena.substring(4,15)
+	alert(substring)
+
+
 });
 
 /***********************************************************************************/
 
 document.getElementById('ex04').addEventListener("click", function() {
-	// ------------------
-	// Codi exercici 4...
-	// ------------------
-});
+	
+	let cadena = prompt("Introdueix una cadena de text: ")
+	let letter = prompt("Introdueix una lletra: ")
+	let total = 0
 
+	if (cadena.includes(letter)) {
+		for (let i = 0; i < cadena.length; i++) {
+			if (cadena[i] == letter) {
+				total += 1;
+			}
+    }
+    alert("El total de vegades de la lletra " + letter + " a la cadena " + cadena + " és " + total);
+	} else {
+		alert("La lletra " + letter + " no existeix a la paraula " + cadena);
+	}
+})
 /***********************************************************************************/
 
 document.getElementById('ex05').addEventListener("click", function() {
-	// ------------------
-	// Codi exercici 5...
-	// ------------------
+
+	let email = prompt("Introdueix una adreça de correu electronic: ")
+	let sign = "@"
+	let total = 0
+	if (email.includes(sign)) {
+		for (let i = 0; i < email.length; i++) {
+			if (email[i] === sign) {
+				total += 1;
+			}
+		if (total > 1) {
+			alert("Una adreça de correu no pot tenir més d'un caracter @")
+		}else{
+			alert("L'adreça de correu especificada és correcta")
+		}
+    }
+	
+	}else {
+
+		alert("Això no és un email!")
+	}
+
+
 });
 
 /***********************************************************************************/
 
 document.getElementById('ex06').addEventListener("click", function() {
-	// ------------------
-	// Codi exercici 6...
-	// ------------------
+	
+
+
+
 });
 
 /***********************************************************************************/
